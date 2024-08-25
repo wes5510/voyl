cfd4a21d84d452aeecfd6a024874c8effb01ab19
wes5510 <wes5510@icloud.com>
2024-08-25 17:59:26 +0900

    feat: add layout for home page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,11 @@
+import { css } from '../../../styled-system/css/index.mjs'
+import { vstack } from '../../../styled-system/patterns/index.mjs'
+
 export default function App(): JSX.Element {
-  return <div>App</div>
+  return (
+    <div className={vstack({ gap: 0, h: 'full' })}>
+      <div className={css({ h: 10, w: 'full' })}>Header</div>
+      <div className={css({ flex: 1, w: 'full' })}>Body</div>
+    </div>
+  )
 }
