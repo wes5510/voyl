348e1295245378d880592839a979ef72869286f0
wes5510 <wes5510@icloud.com>
2024-09-08 17:57:25 +0900

    feat: Add IconButton component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,50 @@
 import ListIcon from './components/icon/ListIcon'
 import Breadcrumb from './components/Breadcrumb'
+import { center, hstack, vstack } from '@styled-system/patterns'
+import { MouseEventHandler, ReactNode } from 'react'
 import { css } from '@styled-system/css'
-import { vstack } from '@styled-system/patterns'
+
+function IconButton({
+  icon,
+  active,
+  onClick
+}: {
+  icon: ReactNode
+  active?: boolean
+  onClick?: MouseEventHandler
+}): JSX.Element {
+  return (
+    <button
+      className={center({
+        w: 10,
+        h: 10,
+        cursor: 'pointer',
+        bg: active
+          ? 'zinc.200'
+          : {
+              _hover: 'zinc.100'
+            }
+      })}
+      onClick={onClick}
+    >
+      {icon}
+    </button>
+  )
+}
 
 export default function App(): JSX.Element {
   return (
     <div className={vstack({ gap: 0, h: 'full' })}>
-      <div className={css({ h: 10, w: 'full' })}>
+      <div
+        className={hstack({
+          paddingStart: 2.5,
+          h: 10,
+          w: 'full',
+          borderBottomWidth: '1px',
+          borderBottomColor: 'black',
+          justify: 'space-between'
+        })}
+      >
         <Breadcrumb
           items={[
             {
@@ -17,7 +55,22 @@ export default function App(): JSX.Element {
           ]}
         />
       </div>
-      <div className={css({ flex: 1, w: 'full' })}>Body</div>
+      <div className={hstack({ flex: 1, w: 'full', gap: 0 })}>
+        <div
+          className={vstack({
+            h: 'full',
+            borderRightWidth: '1px',
+            borderRightColor: 'black',
+            justify: 'space-between'
+          })}
+        >
+          <div className={vstack({ gap: 0 })}>
+            <IconButton active icon={<ListIcon />} />
+            <IconButton icon={<ListIcon />} />
+          </div>
+        </div>
+        <div className={css({ flex: 1 })}>Main</div>
+      </div>
     </div>
   )
 }
