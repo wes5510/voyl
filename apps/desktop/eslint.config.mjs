798cf48047055a74bd6256727af128ecda89d61e
wes5510 <wes5510@icloud.com>
2025-07-06 16:11:02 +0900

    chore: add react query

--- apps/desktop/eslint.config.mjs
+++ apps/desktop/eslint.config.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
 import voylPlugin from '@voyl/eslint-plugin-voyl'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -17,6 +18,7 @@ export default [
   {
     ignores: ['**/node_modules', '**/dist', '**/out', '**/.gitignore'],
   },
+  ...pluginQuery.configs['flat/recommended'],
   ...fixupConfigRules(
     compat.extends(
       'eslint:recommended',
--- apps/desktop/src/renderer/main.tsx
+++ apps/desktop/src/renderer/main.tsx
@@ -4,11 +4,16 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router'
 import IndexPage from './pages'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <IndexPage />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <IndexPage />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
